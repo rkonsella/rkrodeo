9dfd4e07a61b849f9401837d344522abec07e8a0
Riley <konsellr@union.edu>
2017-05-11 14:17:15 -0400

    hope this doesn't ruin everything

---
+++ app.js
@@ -0,0 +1,49 @@
+/**
+ * @ngdoc overview
+ * @name app
+ * @description
+ * # app
+ *
+ * Main module of the application.
+ */
+(function () {
+    'use strict';
+    angular
+        .module('app', [
+            'ngAnimate',
+            'ngResource',
+            'ngSanitize',
+            'ngTouch',
+            'ngStorage',
+            'ngStore',
+            'ui.router',
+            'ui.utils',
+            'ui.load',
+            'ui.jp',
+            'oc.lazyLoad'
+        ]);
+
+
+    var routerApp = angular.module('rodeo', ['ui.router']);
+
+    routerApp.config(function ($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise('/home');
+
+        $stateProvider
+
+            // HOME STATES AND NESTED VIEWS ========================================
+            .state('home', {
+                url: '/home',
+                templateUrl: 'states/test.html'
+            })
+
+            // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+            // .state('about', {
+            //     // we'll get to this in a bit       
+            // });
+
+    });
+
+
+})();
