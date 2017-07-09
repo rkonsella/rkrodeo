926f06e7c2ee76bf518fa13bc1d50fed09b436e4
Riley <konsellr@union.edu>
2017-07-09 15:57:25 -0400

    where am i

--- controllers/home.js
+++ controllers/home.js
@@ -1,28 +1,6 @@
-(function() {
-    'use strict';
-    angular
-        .module('rodeo')
-        .controller('HomeCtrl', HomeCtrl);
-
-        function HomeCtrl($scope) {
-		
-			$scope.color = "#fefefe";
-
-
-			$scope.location = {
-				city: 		"Schenectady",
-				state: 		"New York",
-				country: 	"USA"	
-			};
-
-			function getMapsLink(){
-				return "http://maps.google.com/?q=" +
-					$scope.location.city + ", " +
-					$scope.location.state + " " +
-					$scope.location.country;
-			}
-
-
-
-		}
-}());
+function HomeCtrl($scope){
+    $scope.count = 0;
+    $scope.increment = function (){
+        $scope.count = $scope.count + 1;
+    }
+}
