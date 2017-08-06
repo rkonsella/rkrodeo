4d88231a4bfd1b31a5fcc92e5a441e9232fe289e
Riley <konsellr@union.edu>
2017-08-06 16:39:09 +0200

    embedded twitter js test[A

--- app.js
+++ app.js
@@ -50,20 +50,25 @@
 				templateUrl: 'states/watson/countries/germany.html'
 			});
 	});
+	app.filter("trust", ['$sce', function ($sce) {
+		return function (htmlCode) {
+			return $sce.trustAsHtml(htmlCode);
+		}
+	}]);
 	app.controller("GlobalCtrl", function () {
 	});
 	app.controller("HomeCtrl", function () {
 	});
-	app.controller("WatsonCtrl", function ($scope,$state) {
+	app.controller("WatsonCtrl", function ($scope, $state) {
 		this.currentState = $state.current.name;
 		this.testVal = "this is a string for testing";
-		this.stateName = function(){
+		this.stateName = function () {
 			var name = this.currentState;
 			name = name.substring(7);
-			if (name == "landing"){
+			if (name == "landing") {
 				name = "watson";
 			}
-			return(name);
+			return (name);
 		};
 		this.twitterTest = '<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">ME: sure, but how often do you come across a good peephole?<br><br>HER: I asked if you were a &quot;people&quot; person<br><br>ME: ohhh...definitely not</p>&mdash; Elvish Presley (@_ElvishPresley_) <a href="https://twitter.com/_ElvishPresley_/status/891379632112533505">July 29, 2017</a></blockquote><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>'
 	});
