ae00949e8451c3bb084cd0838376542bbb4e3d4f
Riley <konsellr@union.edu>
2018-04-11 15:23:38 +0700

    days this year func

--- app.js
+++ app.js
@@ -114,7 +114,7 @@
 		this.daysOnWatson = function () {
 			var start = new Date(2017,7,13);
 			var today = new Date();
-			var days = Math.floor(today - start)/1000/60/60/24
+			var days = Math.floor((today - start)/1000/60/60/24);
 			return (days);
 		};
 	});
